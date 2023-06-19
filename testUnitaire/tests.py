53572efc5a26c73588c3c2dcf3d341dee91b32ff
Quentin PAYET <quentinpayet1203@gmail.com>
2023-06-19 13:06:27 +0400

    unit test with db

--- testUnitaire/tests.py
+++ testUnitaire/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from testUnitaire.models import Person
 
 # Create your tests here.
 
@@ -20,3 +21,13 @@ class SimpleCalcul(TestCase):
         """Simple addition"""
         self.assertEqual(1 + 1, 2)
         self.assertEqual(2 + 2, 22)
+
+
+class SimpleTestDB(TestCase):
+    def setUp(self):
+        Person.objects.create(first_name="jean", last_name="wick")
+
+    def test_database(self):
+        """Simple test database"""
+        cereal_killeur = Person.objects.get(first_name="jean")
+        self.assertEqual(cereal_killeur.last_name, "wicked")
